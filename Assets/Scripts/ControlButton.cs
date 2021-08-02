4430ca1a1ea2ee40423a3c3fdb1b959703639a04
Walugus-Grudenburg <superstinkyfarts@gmail.com>
2021-08-01 20:26:55 -0400

    Added Quit Button

---
+++ Assets/Scripts/ControlButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlButton : MonoBehaviour
+{
+    public Text text;
+    public bool AxisMode;
+    public string input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AxisMode) { 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
